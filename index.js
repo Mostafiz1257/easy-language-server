91b4aeeb6e68bc4bac0eb5eacff0484ae6bf127f
Mostafiz1257 <mostafiz1257@gmail.com>
2023-06-10 02:13:26 +0600

    separate admin

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorize access' })
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error: true, message: 'unauthorize access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -36,7 +49,7 @@ async function run() {
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      console.log(req.body);
+      console.log('abx',req.body);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '1h'
       })
@@ -63,7 +76,7 @@ async function run() {
 
     app.post('/users', async (req, res) => {
       const user = req.body;
-      console.log(user);
+      console.log('a',user);
       const query = { email: user.email }
       const exitingUSer = await userCollection.findOne(query)
       if (exitingUSer) {
@@ -73,9 +86,19 @@ async function run() {
       res.send(result)
     })
 
+
+    app.get('/users/admin/:email',  async(req,res)=>{
+      const email = req.params.email;    
+      const query = { email: email}
+      const user=  await userCollection.findOne(query)
+      const result = {admin : user?.role ==='admin'}
+      console.log('hit this');
+      res.send(result)
+    })
+
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id
-      console.log(id);
+      console.log('b',id);
       const filter = { _id: new ObjectId(id) }
       const updateDoc = {
         $set: {
@@ -87,7 +110,7 @@ async function run() {
     })
     app.patch('/users/instructor/:id', async (req, res) => {
       const id = req.params.id
-      console.log(id);
+      console.log('c',id);
       const filter = { _id: new ObjectId(id) }
       const updateDoc = {
         $set: {
@@ -99,10 +122,14 @@ async function run() {
     })
 
     //select class by email
-    app.get('/myClass', async (req, res) => {
+    app.get('/myClass', verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([])
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
       }
       const query = { email: email }
       const result = await classCollection.find(query).toArray()
